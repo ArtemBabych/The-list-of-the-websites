c337b25fef22e43fbb1de6bef0c2780b81d0d4ff
ArtemBabych <babychartemhome@gmail.com>
2024-01-27 23:37:08 +0200

    Added 2 little practice projects

---
+++ Websites/Practice-Projects/JavaScript/Dog-Years/Dog-Years.js
@@ -0,0 +1,32 @@
+// Our human is 50 years old
+let humanAge = 50;
+// The first two years score more. So, we need a separate calculation for it.
+let earlyYears = 2;
+earlyYears *= 10.5;
+// Here, we calculate the other years that left from a full number.
+let laterYears = humanAge - 2;
+// After this calculation we'll know how many years will he have if he were a dog at those years.
+laterYears *= 4;
+// Let's find out the result for our research. How old would this man be if he were a dog?
+hisAgeInDogYears = earlyYears + laterYears;
+// Alter to lowercase his name.
+let hisName = "Diego".toLowerCase();
+// The finish line of our journey will be displayed on the terminal.
+console.log(`His name is ${hisName}. He is ${humanAge} years old in human years which is ${hisAgeInDogYears} years old in dog years.`);
+
+/* Let's take a look to one more subject */
+// Our human is 20 years old
+let Age = 20;
+// The first two years score more. So, we need a separate calculation for it.
+let eY = 2;
+eY *= 10.5;
+// Here, we calculate the other years that left from a full number.
+let lY = Age - 2;
+// After this calculation we'll know how many years will he have if he were a dog at those years.
+lY *= 4;
+// Let's find out the result for our research. How old would this man be if he were a dog?
+InDogYears = eY + lY;
+// Alter to lowercase his name.
+let Name = "Pedro".toLowerCase();
+// The finish line of our journey will be displayed on the terminal.
+console.log(`His name is ${Name}. He is ${Age} years old in human years which is ${InDogYears} years old in dog years.`);
